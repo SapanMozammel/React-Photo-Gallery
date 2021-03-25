e19589d3fbbae95dfbaef8f9a53c30f4e039e474
Sapan Mozammel <sapanmozammel@gmail.com>
2021-03-25 18:56:06 +0600

    masonry layout setup

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,19 +1,32 @@
 import React, { useEffect, useState } from 'react';
-import JsonData from "./../../data/images.json";
+import JsonData from './../../data/images.json';
+import Masonry from 'react-masonry-css';
 const Gallery = () => {
-    const [images, setImages] = useState([])
-    
-    useEffect(() => {
-        setImages(JsonData)
-      }, []); // Only re-run the effect if count changes
-    
-    return (
-        <div className="PG-topbar-gallery">
-            {images.map( (img, index) => (
-                <img src={img.url} alt={`Images ${index}`} key={index} />
-            ))}
-        </div>
-    )
-}
+	const [images, setImages] = useState([]);
+	const breakpointColumnsObj = {
+		default: 4,
+		1100: 3,
+		700: 2,
+		500: 1,
+	};
+
+	useEffect(() => {
+		setImages(JsonData);
+	}, []);
+
+	return (
+		<Masonry
+			breakpointCols={breakpointColumnsObj}
+			className='PG-masonry-gallery'
+			columnClassName='PG-masonry-column'>
+			{images.map((img, index) => (
+				<div className="PG-masonry-column">
+					<img src={img.url} alt={img.alt} key={index} />
+					<h2>{index}</h2>
+				</div>
+			))}
+		</Masonry>
+	);
+};
 
 export default Gallery;
