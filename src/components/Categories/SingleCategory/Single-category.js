20fab6c7bdae60d0bafa788c77e10ca416aeea43
Sapan Mozammel <sapanmozammel@gmail.com>
2021-03-31 16:08:45 +0600

    category related bug fix

--- src/components/Categories/SingleCategory/Single-category.js
+++ src/components/Categories/SingleCategory/Single-category.js
@@ -3,14 +3,13 @@ import { Link } from 'react-router-dom';
 
 const SingleCategory = ({ category, index, filterCategoryWise }) => {
 	return (
-		<Link
-			to={`/${category.item}`}
+		<div
 			key={index}
 			style={{ backgroundImage: `url(${category.parent.url})` }}
 			onClick={() => filterCategoryWise(category)}
 			className='single-category'>
 			<h3>{category.item}</h3>
-		</Link>
+		</div>
 	);
 };
 
